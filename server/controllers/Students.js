ca8183763b322f4458d3bad93e193a0ce63837e3
Pavankumar Guttedar <pavankumar494748@gmail.com>
2024-01-30 21:49:45 +0530

    new routes

--- server/controllers/Students.js
+++ server/controllers/Students.js
@@ -5,12 +5,132 @@ exports.getStudents = async (req, res) => {
     // get students data
     const data = pool.query("SELECT * FROM students", (error, results) => {
       if (error) throw error;
-      res.status(200).json({
+      return res.status(200).json({
         success: true,
-        message: "Students data fetched true",
+        message: "Students data fetched success",
         data: results.rows,
       });
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.getStudentById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    pool.query(
+      "SELECT * FROM students WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (error) throw error;
+        return res.status(200).json({
+          success: true,
+          message: "Student data fetched success",
+          data: results.rows,
+        });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.addStudent = async (req, res) => {
+  try {
+    // get data
+    const { name, email, age, dob } = req.body;
+    // validate
+    if (!name || !email || !age || !dob) {
+      return res.status(401).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+    // check if user exists with given email
+    pool.query(
+      "SELECT * FROM students WHERE email = $1",
+      [email],
+      (error, results) => {
+        if (results.rows.length) {
+          return res.status(401).json({
+            success: false,
+            message: "User already exists with this email",
+          });
+        }
+
+        // add students to db
+        pool.query(
+          "INSERT INTO students (name, email,age, dob) VALUES($1,$2,$3,$4)",
+          [name, email, age, dob],
+          (error, results) => {
+            if (error) {
+              console.log(error);
+              return res
+                .status(500)
+                .json({ success: false, message: "Error while adding to db" });
+            }
+            // else return success
+            return res.status(201).json({
+              success: true,
+              message: "Student added",
+              data: results.rows,
+            });
+          }
+        );
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+exports.deleteStudent = async (req, res) => {
+  try {
+    // get id
+    const id = parseInt(req.params.id);
+    // write query
+    pool.query(
+      "SELECT * FROM students WHERE id = $1",
+      [id],
+      (error, results) => {
+        if (!results.rows.length) {
+          return res
+            .status(404)
+            .json({ success: false, message: "Student not found" });
+        }
+
+        // delete student
+        pool.query(
+          "DELETE FROM students WHERE id = $1",
+          [id],
+          (error, results) => {
+            if (error) {
+              return res
+                .status(404)
+                .json({ success: false, message: "Could not delete student" });
+            }
+            // return success
+            return res.status(200).json({
+              success: true,
+              message: "Student deleted",
+            });
+          }
+        );
+      }
+    );
   } catch (error) {
     console.log(error);
     res.status(500).json({
--- server/routes/studentsRoutes.js
+++ server/routes/studentsRoutes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getStudents } = require("../controllers/Students");
+const {
+  getStudents,
+  getStudentById,
+  addStudent,
+  deleteStudent,
+} = require("../controllers/Students");
 
 router.route("/").get(getStudents);
+router.route("/:id").get(getStudentById);
+router.route("/").post(addStudent);
+router.route("/:id").delete(deleteStudent);
 
 module.exports = router;
